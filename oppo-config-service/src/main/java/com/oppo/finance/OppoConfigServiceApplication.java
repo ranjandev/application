7bf6104c215138f83a4e3d09b3c2fd93e8525404
ranjandev <rakes16ranjan@gmail.com>
2020-04-22 12:02:48 +0530

    first commit

---
+++ oppo-config-service/src/main/java/com/oppo/finance/OppoConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oppo.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppoConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppoConfigServiceApplication.class, args);
+	}
+
+}
