fb77ca06c6d57432ddc5f164a2e6f5f333d4ae65
ranjandev <rakes16ranjan@gmail.com>
2020-04-23 17:13:05 +0530

    create creditvidya service

---
+++ oppo-creditvidya-service/src/main/java/com/oppo/finance/OppoCreditvidyaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.oppo.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppoCreditvidyaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppoCreditvidyaServiceApplication.class, args);
+	}
+
+}
---
+++ oppo-creditvidya-service/src/test/java/com/oppo/finance/OppoCreditvidyaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oppo.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OppoCreditvidyaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
